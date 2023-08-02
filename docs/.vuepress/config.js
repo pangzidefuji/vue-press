92f9ea8418fd4b831b3e0e25f425d477484aaf82
pangzidefuji <pangzidefuji@sina.com>
2023-08-02 21:58:18 +0800

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Chain door wallet',
+    description: '',
+    themeConfig: {
+        navbar: [
+            { text: 'Terms of Service', link: '/' },
+            { text: 'Privacy Policy', link: '/privacy-policy' },
+        ],
+        sidebar: [
+            '/',
+            '/privacy-policy'
+        ],
+    }
+  }
