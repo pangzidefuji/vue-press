ca2745a807982f955837a4820ebfecd13a30dd22
pangzidefuji <91173718+pangzidefuji@users.noreply.github.com>
2023-10-07 14:58:39 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,15 @@
 module.exports = {
   title: 'Chain door wallet',
   themeConfig: {
-    nav: [ // 导航栏链接
-      { text: 'Terms of Service', link: '/' }, // 首页链接
-      { text: 'Privacy Policy', link: '/privacy-policy' }, // 隐私政策页面链接
-      // 添加更多的链接...
-    ],
+    
     sidebar: [ // 侧边栏
       '/',
       '/privacy-policy',
     ],
+    nav: [ // 导航栏链接
+      { text: 'Terms of Service', link: '/' }, // 首页链接
+      { text: 'Privacy Policy', link: '/privacy-policy' }, // 隐私政策页面链接
+      // 添加更多的链接...
+    ]
   },
 }
