2e96dad5724a3a69ac3f1d1c9f45529fc4a44c6e
pangzidefuji <91173718+pangzidefuji@users.noreply.github.com>
2023-10-07 14:55:57 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,12 @@ module.exports = {
   themeConfig: {
     nav: [ // 导航栏链接
       { text: 'Terms of Service', link: '/' }, // 首页链接
-      { text: 'Privacy Policy', link: '/privacy-policy/' }, // 隐私政策页面链接
+      { text: 'Privacy Policy', link: '/privacy-policy' }, // 隐私政策页面链接
       // 添加更多的链接...
     ],
     sidebar: [ // 侧边栏
       '/',
-      '/privacy-policy/',
+      '/privacy-policy',
     ],
   },
 }
